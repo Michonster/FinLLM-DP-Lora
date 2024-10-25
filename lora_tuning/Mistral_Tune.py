652e745b3d1ceb241f34457a6fd1980da02615ca
rgtdthrd <beik@rpi.edu>
2024-10-25 19:40:24 -0400

    training part

--- lora_tuning/Mistral_Tune.py
+++ lora_tuning/Mistral_Tune.py
@@ -1,11 +1,15 @@
 import torch
 import os
+import wandb, os
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 import torch
 import torch.nn as nn
 from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, BitsAndBytesConfig
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from huggingface_hub import login
+import transformers
+from datetime import datetime
+
 TOKEN = "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"
 login(token = TOKEN)
 
@@ -124,3 +128,51 @@ print_trainable_parameters(model)
 
 # Apply the accelerator. You can comment this out to remove the accelerator.
 model = accelerator.prepare_model(model)
+
+wandb.login()
+
+wandb_project = "viggo-finetune"
+if len(wandb_project) > 0:
+    os.environ["WANDB_PROJECT"] = wandb_project
+
+if torch.cuda.device_count() > 1: # If more than 1 GPU
+    model.is_parallelizable = True
+    model.model_parallel = True
+
+
+project = "viggo-finetune"
+base_model_name = "mistral"
+run_name = base_model_name + "-" + project
+output_dir = "./" + run_name
+
+tokenizer.pad_token = tokenizer.eos_token
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_val_dataset,
+    args=transformers.TrainingArguments(
+        output_dir=output_dir,
+        warmup_steps=5,
+        per_device_train_batch_size=2,
+        gradient_checkpointing=True,
+        gradient_accumulation_steps=4,
+        max_steps=1000,
+        learning_rate=2.5e-5, # Want about 10x smaller than the Mistral learning rate
+        logging_steps=50,
+        bf16=True,
+        optim="paged_adamw_8bit",
+        logging_dir="./logs",        # Directory for storing logs
+        save_strategy="steps",       # Save the model checkpoint every logging step
+        save_steps=50,                # Save checkpoints every 50 steps
+        evaluation_strategy="steps", # Evaluate the model every logging step
+        eval_steps=50,               # Evaluate and save checkpoints every 50 steps
+        do_eval=True,                # Perform evaluation at the end of training
+        report_to="wandb",           # Comment this out if you don't want to use weights & baises
+        run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}"          # Name of the W&B run (optional)
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+trainer.train()
